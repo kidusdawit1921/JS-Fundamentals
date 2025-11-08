f9e914238e86aa5b223f61862ce3ac5f2ca252d9
Kidus Dawit <kidusdawit1921@gmail.com>
2025-11-08 14:27:12 +0300

    Fix console log messages for argument detection

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,7 +2,7 @@ const args = process.argv.slice(2);
 if (args.length === 0) {
     console.log("No arguments");
 } else if (args.length === 1) {
-    console.log("argument found");
+    console.log("Argument found");
 } else {
-    console.log("arguments found");
+    console.log("Arguments found");
 }

ac6125e11637886cd6e48a837dc4cd2fd86ff99b
Kidus Dawit <kidusdawit1921@gmail.com>
2025-11-08 16:25:50 +0300

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,4 +1,4 @@
 const args= process.argv;
-const firstArg = args[2]
-const secondArg = args[3]
+const firstArg = args[2] ? args[2] : "pythonfun";
+const secondArg = args[3] ? args[3] : "HBTN";
 console.log(firstArg + "is" + secondArg);

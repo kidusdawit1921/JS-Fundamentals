c763fca505e4503624e9dae5a3435c6cddf534f4
Kidus Dawit <kidusdawit1921@gmail.com>
2025-11-08 14:25:22 +0300

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,7 +2,7 @@ const args = process.argv.slice(2);
 if (args.length === 0) {
     console.log("No arguments");
 } else if (args.length === 1) {
-    console.log("argument found: ");
+    console.log("argument found");
 } else {
     console.log("arguments found");
 }

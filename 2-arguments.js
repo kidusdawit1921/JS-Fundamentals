2634550744523d2bc307f6920add0f0f6161497c
Kidus Dawit <KidusDawit1921@gmail.com>
2025-11-08 14:16:49 +0300

    Add missing file

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+const args = process.argv.slice(2);
+if (args.length === 0) {
+    console.log("No arguments");
+} else if (args.length === 1) {
+    console.log("argument found: ");
+} else {
+    console.log("arguments found");
+}
